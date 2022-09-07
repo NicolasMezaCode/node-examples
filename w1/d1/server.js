0cdb73fb010a3543cb2518994604d5d328b2bb51
Arthur Di Lascio <arthur.dilascio@gmail.com>
2022-09-07 15:56:38 -0700

    w1d1 first part

--- w1/d1/server.js
+++ w1/d1/server.js
@@ -1,9 +1,26 @@
 const http = require("http");
 
+const data = { message: "Hello World!" };
+
 const server = http.createServer((req, res) => {
   console.log("request received", req.url);
-  res.setHeader("Content-Type", "text/html");
-  res.write("<h1>Hello World!!!!!!!</h1>");
+  if (req.url === "/dogs") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(
+      "<img src='https://i.pinimg.com/originals/b9/7d/97/b97d976b20e9889918becf0f5f4e7cbd.jpg'>"
+    );
+  }
+  if (req.url === "/cats") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(
+      "<img src='https://www.rd.com/wp-content/uploads/2021/01/GettyImages-1175550351.jpg'>"
+    );
+  }
+  if (req.url === "/hello") {
+    res.setHeader("Content-Type", "application/json");
+    res.write(JSON.stringify(data));
+  }
+
   res.end();
 });
 
