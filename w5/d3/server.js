333dd362b7893230bea952784435be15cfa421cf
Arthur Di Lascio <arthur.dilascio@gmail.com>
2022-10-06 14:51:06 -0700

    w5d3

---
+++ w5/d3/server.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+const moment = require("moment");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const port = 3000;
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  const { username, room } = socket.handshake.query;
+
+  socket.join(room);
+
+  console.log("A client has connected", username);
+
+  socket.on("message", (message) => {
+    console.log("message in server", message);
+    io.to(room).emit("message", {
+      username,
+      text: message,
+      time: moment().format("hh:mm a"),
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A client has disconnected");
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
